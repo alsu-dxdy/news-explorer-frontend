f6f27a89548ed5ff6813f3928b85fd391dc0776b
Alsu Abdrakhmanova <dxdy10@mail.ru>
2020-07-05 14:36:37 +0500

    edit: Article create without insertAdjacentHTML

--- src/js/components/Article.js
+++ src/js/components/Article.js
@@ -11,29 +11,54 @@ export default class Article {
     return articleDate;
   }
   create(item, keyword) {
-    const articleCard = document.createElement('div');
     const rusDate = this._changetoRus(item.publishedAt);
+    const articleCard = document.createElement('div');
     articleCard.classList.add('article-card');
-    articleCard.insertAdjacentHTML(
-      'beforeend',
-      `
-      <div class="article-card__image-container">
-        <div class="article-card__image">
-        <div class="article-card__hint-container">
-            <p class="article-card__hint-text">Войдите, чтобы сохранять статьи</p>
-        </div>
-        <button class="article-card__like-icon"></button>
-        </div>
+    // верхняя часть карточки
+    const imageElement = document.createElement('div');
+    const hintContainerElement = document.createElement('div');
+    const hintTextElement = document.createElement('p');
+    const likeButtonElement = document.createElement('button');
 
-      <div class="article-card__description">
-        <p class="article-card__date">${rusDate}</p>
-    <h3 class="article-card__title">${item.description}</h3>
-    <p class="article-card__text">${item.content}</p>
-    <p class="article-card__source">${item.source.name}</p>
-      </div >
-    </div >
-      `,
-    );
+    hintContainerElement.appendChild(hintTextElement);
+    imageElement.appendChild(hintContainerElement);
+    imageElement.appendChild(likeButtonElement);
+
+    // нижняя часть карточки
+    const descriptionElement = document.createElement('div');
+    const dateElement = document.createElement('p');
+    const titleElement = document.createElement('p');
+    const textElement = document.createElement('p');
+    const sourceElement = document.createElement('p');
+
+    descriptionElement.appendChild(dateElement);
+    descriptionElement.appendChild(titleElement);
+    descriptionElement.appendChild(textElement);
+    descriptionElement.appendChild(sourceElement);
+
+    // соединение 2х частей в целую карточку
+    articleCard.appendChild(imageElement);
+    articleCard.appendChild(descriptionElement);
+
+    // классы
+    imageElement.classList.add('article-card__image');
+    hintContainerElement.classList.add('article-card__hint-container');
+    hintTextElement.classList.add('article-card__hint-text');
+    likeButtonElement.classList.add('article-card__like-icon');
+
+    descriptionElement.classList.add('article-card__description');
+    dateElement.classList.add('article-card__date');
+    titleElement.classList.add('article-card__title');
+    textElement.classList.add('article-card__text');
+    sourceElement.classList.add('article-card__source');
+
+    // textContent-ы
+    dateElement.textContent = `${rusDate}`;
+    titleElement.textContent = `${item.description}`;
+    textElement.textContent = `${item.content}`;
+    sourceElement.textContent = `${item.source.name}`;
+    hintContainerElement.textContent = 'Войдите, чтобы сохранять статьи';
+
     articleCard.querySelector('.article-card__image').style.backgroundImage = `url(${item.urlToImage})`;
     articleCard.setAttribute('src', `${item.url}`);
     articleCard.setAttribute('keyword', `${keyword}`);
